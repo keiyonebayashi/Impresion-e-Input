2cc28ce89212add66f4024fdc9d342fd0ba2b8b3
A$AP Treize <160423988+asaptreize@users.noreply.github.com>
2024-09-29 20:34:21 -0600

    Impresión e Input en una variable
    Ejercicio de programación en "python", para impresión de la palabra y captura de dato en input.

---
+++ impresion-e-input.py
@@ -0,0 +1,8 @@
+# Imprimir el texto
+print("¿Cuál es tu nombre?")
+
+# Capturar el texto ingresado por el usuario
+nombre = input()
+
+# Imprimir el texto junto con "Soy Tuzo"
+print("Hola, soy " + nombre + ", y Soy Tuzo!")
